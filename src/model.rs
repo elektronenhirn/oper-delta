86b9c48c68597ec6b3e0fdbb43877217179c0990
Florian Bramer <florian.bramer@esrlabs.com>
2020-11-10 17:41:22 +0100

    Optimized model creation
    We don't need to calc the distance when
    the target branch does not exist anyway

--- src/model.rs
+++ src/model.rs
@@ -180,12 +180,15 @@ fn calc_branch_deltas_for_a_single_repo(
 
             Some(BranchDelta {
                 branch_name: String::from(*branch_name),
-                delta,
-                distance_head_to_merge_base: calc_distance_head_to_merge_base(&repo, branch_name),
-                distance_target_to_merge_base: calc_distance_target_to_merge_base(
-                    &repo,
-                    branch_name,
-                ),
+                delta: delta.clone(),
+                distance_head_to_merge_base:  match delta {
+                    Delta::BranchNotFound => Ok(0),
+                    _ => calc_distance_head_to_merge_base(&repo, branch_name),
+                },
+                distance_target_to_merge_base:  match delta {
+                    Delta::BranchNotFound => Ok(0),
+                    _ => calc_distance_target_to_merge_base(&repo, branch_name),
+                },
             })
         })
         .collect::<Vec<_>>();
